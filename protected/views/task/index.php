7559c40d736d49a0e989d3a7d6bbb66d544a47a0
PalPav <palpav@yandex.ru>
2013-03-22 22:46:26 +0400

    favicon

--- protected/views/task/index.php
+++ protected/views/task/index.php
@@ -9,25 +9,47 @@ jQuery(document).ready(function(){
         $(this).toggleClass('task-head-opened');
         $(this).next().slideToggle('fast');
     });
+
+    $('.task-list').on('click','input[name="add_subtask"]', function(event) {
+        event.stopPropagation();
+    });
+
+    $('.task-list').on('click','input[name="edit_task"]', function(event) {
+        event.stopPropagation();
+    });
+
+    $('.task-list').on('click','input[name="task_menu"]', function(event) {
+        event.stopPropagation();
+
+    });
+
+
+
+
+
+
+
 });
 </script>
 
 
 <h3>Лента задач (Жуй хомяк, жуй)</h3>
-<?php
+
+    <?php
+
 
 
 echo "<div class='task-list'>";
 foreach ($data as $key => $task) {
-    echo "<div class='task-head level-".$task['priority_level_id']."'>
+    echo "<div class='task-head task-level-".$task['priority_level_id']."'>
             <div class='head-controls'>
-                <input type='button' value='+' title='Добавить подзадачу'>
-                <input type='button' value='E' title='Редактировать задачу'>
-                <input type='button' value='M' title='Редактировать статус'>
+                <input type='button' name='add_subtask' value='+' title='Добавить подзадачу'>
+                <input type='button' name='edit_task' value='E' title='Редактировать задачу'>
+                <input type='button' name='task_menu' value='M' title='Редактировать статус'>
             </div>
             <div class='head-time'>".$task['end_time']."</div>
             <div class='head-title'>#".$task['id']." ".$task['subject']."</div>
             </div>";
-    echo "<div class='task-container'><br><br> ".$task['body']."<br><br></div>";
+    echo "<div class='task-container'> ".$task['body']."</div>";
 }
 echo "</div>";
